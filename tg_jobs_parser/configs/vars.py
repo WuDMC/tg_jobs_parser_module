148ba2d656c79c9810d2d496297809ff8e3992ad
Denis Mironov <mironovdenis@list.ru>
2024-06-17 16:37:36 +0300

    max limit = 500

--- tg_jobs_parser/configs/vars.py
+++ tg_jobs_parser/configs/vars.py
@@ -3,7 +3,7 @@ GCS_BUCKET_NAME = 'wu-eu-west'
 CHANNEL_METADATA_PATH = 'job_parser/channels.json'
 MSGS_DATA_PATH = 'job_parser/msgs'
 START_DATE = '2024-05-01'
-MAX_LIMIT = 20
+MAX_LIMIT = 500
 ACCOUNT_NAME = 'airflow_account'
 
 
